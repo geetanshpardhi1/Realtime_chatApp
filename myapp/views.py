89269b80c46f29c7d4a8159c03d8a3b6dab95253
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2023-10-18 20:10:33 +0530

    created chatroom model and views to see rooms in index page

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import ChatRoom
+# Create your views here.
+def index(request):
+    chatrooms = ChatRoom.objects.all()
+    return render(request,'myapp/index.html',{'chatrooms':chatrooms})
+
+
+def detail()
